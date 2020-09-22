b057d54b050ce501dcc0207ecf710c587558e143
hoangph3 <hoangph3@pop-os.localdomain>
2020-09-23 01:01:59 +0700

    xong-chuc-nang-bai-tap

--- download.php
+++ download.php
@@ -1,31 +1,24 @@
 <?php
-$nameFile = basename($_GET['file']);
+if(isset($_GET['file']))
+{
+    $filename = $_GET['file'];
 
-$FileExtension = explode(".",$nameFile)[count(explode(".",$nameFile))-1];
+$dir = "uploads/"; 
+$file = $dir . $filename;
 
-$file_url = $_SERVER['DOCUMENT_ROOT'].'/downloadfile/files/'.$nameFile;
-
-if(!empty($filename)){
-    // Check file is exists on given path.
-    if(file_exists($download_file))
+if (file_exists($file))
     {
-      header('Content-Disposition: attachment; filename=' . $filename);  
-      readfile($download_file); 
-      exit;
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
     }
-    else
-    {
-      echo 'File does not exists on given path';
-    }
- }
- ?>
-
- <?php
-$nameFile = "hinh1.jpg"; // thay đổi tên file từ csdl tại đây
-$FileExtension = explode(".",$nameFile)[count(explode(".",$nameFile))-1];
-$file_url = $_SERVER['DOCUMENT_ROOT'].'/downloadfile/files/'.$nameFile;
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=file-".md5(rand(0,1000)).".".$FileExtension); 
-readfile($file_url);
+} 
 ?>
